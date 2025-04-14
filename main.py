8a51c858e7576717cb169762a9274d4def81792a
Klienz <sunmankind@gmail.com>
2025-04-14 21:23:48 +1000

    Fix: removed invalid email/text main.py

--- main.py
+++ main.py
@@ -200,7 +200,6 @@ async def on_message(message):
 print("🟢 Attempting to start Shia...")
 bot.run(TOKEN)
 
-On Mon, Apr 14, 2025 at 8:54 PM Klienz <sunmankind@gmail.com> wrote:
 import discord
 import os
 import json
